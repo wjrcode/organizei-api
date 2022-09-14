4b76f0efcc0762c42414f7d90cb49df3ac231891
Willian <47341070+wjrcode@users.noreply.github.com>
2022-09-14 12:26:17 -0300

    mensagem

--- src/routes/usuarios/addUsuario.js
+++ src/routes/usuarios/addUsuario.js
@@ -8,15 +8,15 @@ module.exports = Router({ mergeParams: true }).post(
 			const { nome, apelido, email, senha } = req.body
 			const { models } = req.db
 
-			if (!nome) return res.status(400).json('Nome não informado!')
-			if (!apelido) return res.status(400).json('Apelido não informado!')
-			if (!email) return res.status(400).json('E-mail não informado!')
-			if (!senha) return res.status(400).json('Senha não informada!')
+			if (!nome) return res.status(202).json({ valido: false, msg: 'Nome não informado!' })
+			if (!apelido) return res.status(202).json({valido: false, msg: 'Apelido não informado!'})
+			if (!email) return res.status(202).json({valido: false, msg: 'E-mail não informado!'})
+			if (!senha) return res.status(202).json({valido: false, msg: 'Senha não informada!'})
 
 			const exists = await models.usuario.findOne({ where: { email } })
 
 			if (exists) {
-				return res.status(400).json('Já existe um usuário cadastrado com esse e-mail!')
+				return res.status(202).json({valido: false, msg: 'Já existe um usuário cadastrado com esse e-mail!'})
 			}
 
 			const salt = bcrypt.genSaltSync(10)
@@ -29,7 +29,7 @@ module.exports = Router({ mergeParams: true }).post(
 				senha: senhaCrypt,
 			})
 
-			return res.status(201).json({ id: usuario.id,  valido: true, msg: 'Usuário criado com sucesso!' })
+			return res.status(201).json({ id: usuario.id, valido: true, msg: 'Usuário criado com sucesso!' })
 		} catch (error) {
 			return next(error)
 		}
