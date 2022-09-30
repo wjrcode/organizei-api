afe4f27ed2b7df9ace7bea6a028f13d6be666c26
Willian <47341070+wjrcode@users.noreply.github.com>
2022-09-30 15:26:05 -0300

    Delete

--- src/routes/tarefas/id/deleteTarefa.js
+++ src/routes/tarefas/id/deleteTarefa.js
@@ -1,27 +1,21 @@
 const Router = require('express').Router
 
-module.exports = Router({ mergeParams: true }).put(
-	'/tarefas/:id/concluir',
+module.exports = Router({ mergeParams: true }).delete(
+	'/tarefas/:id',
 	async (req, res, next) => {
 		try {
 			const { id } = req.params
-			const { concluido } = req.body
 			const { models } = req.db
 
 			if (!id || id == 'null') return res.status(202).json({ valido: false, msg: 'ID não informado!' })
-			if (!concluido) return res.status(202).json({ valido: false, msg: 'Concluído não informado!' })
 
 			const exists = await models.tarefa.findByPk(id)
 
 			if (!exists) return res.status(202).json({ valido: false, msg: 'Tarefa não encontrada!' })
 
-			await models.tarefa.update(
-				{
-					concluido
-				},
-				{ where: { id } })
+			await models.tarefa.destroy({ where: { id } })
 
-			return res.status(201).json({ valido: true, msg: 'Tarefa concluída com sucesso!' })
+			return res.status(201).json({ valido: true, msg: 'Tarefa excluída com sucesso!' })
 		} catch (error) {
 			return next(error)
 		}
