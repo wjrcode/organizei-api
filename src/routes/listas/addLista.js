ba5128539cfe5c3cc4c52fe33a25b81b82cbe9a3
wjrcode <willianjunior012@gmail.com>
2022-10-13 21:15:08 -0300

    listas

--- src/routes/listas/addLista.js
+++ src/routes/listas/addLista.js
@@ -7,9 +7,11 @@ module.exports = Router({ mergeParams: true }).post(
 			const { nome, cor, itens } = req.body
 			const { models } = req.db
 
+			console.log(itens)
+
 			if (!nome) return res.status(202).json({ valido: false, msg: 'Nome não informado!' })
 			if (!cor) return res.status(202).json({ valido: false, msg: 'Cor não informada!' })
-			if (!itens) return res.status(202).json({ valido: false, msg: 'Itens não informados!' })
+			if (!itens || itens.length < 1) return res.status(202).json({ valido: false, msg: 'Itens não informados!' })
 
 			if (cor == 0) return res.status(202).json({ valido: false, msg: 'Cor não informada!' })
 
@@ -23,12 +25,21 @@ module.exports = Router({ mergeParams: true }).post(
 				usuarioId: 1
 			})
 
-			itens.map(async (item) => {
-				await models.item.create({
-					nome: item.nome,
-					listaId: lista.id
+			try {
+				itens.map(async (item) => {
+					await models.item.create({
+						nome: item.nome,
+						listumId: lista.id
+					})
 				})
-			})
+				
+			} catch (error) {
+
+				await models.lista.destroy({ where: { id: lista.id } })
+				return res.status(202).json({ valido: false, msg: 'Erro ao criar lista!' })
+				
+			}
+			
 
 			return res.status(201).json({ id: lista.id, valido: true, msg: 'Lista criada com sucesso!' })
 		} catch (error) {
--- src/sequelize/models/Lista.js
+++ src/sequelize/models/Lista.js
@@ -6,7 +6,7 @@ module.exports = (sequelize) => {
 		{
 			id: { type: DataTypes.BIGINT, allowNull: false, unique: true, index: true, primaryKey: true, autoIncrement: true },
 			nome: { type: DataTypes.STRING(45), allowNull: false },
-			cor: { type: DataTypes.STRING(6), allowNull: false },
+			cor: { type: DataTypes.BIGINT, allowNull: false },
 		},
 		{
 			tableName: 'listas',
