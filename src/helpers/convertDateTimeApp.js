27de5d2ee19d01306e07501fc013c8614170c852
wjrcode <willianjunior012@gmail.com>
2022-10-16 20:30:26 -0300

    hora

--- src/helpers/convertDateTimeApp.js
+++ src/helpers/convertDateTimeApp.js
@@ -1,6 +1,6 @@
 module.exports = (data, mensagem ='' ) => {
 	if (!data) return null
 
-	return `${data.getDate() <10 ? '0' : ''}${data.getDate()}/${data.getMonth()+1 <10 ? '0' : ''}${data.getMonth()+1}/${data.getFullYear()} ${mensagem}${data.getHours()}:${data.getMinutes()}`
+	return `${data.getDate() <10 ? '0' : ''}${data.getDate()}/${data.getMonth()+1 <10 ? '0' : ''}${data.getMonth()+1}/${data.getFullYear()} ${mensagem}${data.getHours() <10 ? '0' : ''}${data.getHours()}:${data.getMinutes() <10 ? '0' : ''}${data.getMinutes()}`
 	//return `${ano.substring(0,4)}-${mes}-${dia} ${ano.substring(5)}`
 }
--- src/routes/projetos/getProjetos.js
+++ src/routes/projetos/getProjetos.js
@@ -21,6 +21,13 @@ module.exports = Router({ mergeParams: true }).get(
 			 projetos.map((projeto)=>{
 				projeto.dataValues.dataInicial = convertDateTime(projeto.dataInicial).substring(0,10)
 				projeto.dataValues.dataFinal = convertDateTime(projeto.dataFinal).substring(0,10)
+
+				projeto.atividade.map((atividade)=>{
+
+					atividade.dataValues.dataInicial = convertDateTime(atividade.dataInicial)
+					atividade.dataValues.dataFinal = convertDateTime(atividade.dataFinal)
+
+				})
 			})
 
 			return res.status(201).json({ projetos })
